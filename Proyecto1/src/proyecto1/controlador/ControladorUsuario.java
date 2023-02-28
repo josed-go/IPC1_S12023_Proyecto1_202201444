f265324e5b517a054a06f9fd675473070588c54d
José Góngora <jdavidgongorao@gmail.com>
2023-02-27 21:54:22 -0600

    Se crearon las validaciones para la contraseña

--- Proyecto1/src/proyecto1/controlador/ControladorUsuario.java
+++ Proyecto1/src/proyecto1/controlador/ControladorUsuario.java
@@ -38,8 +38,28 @@ public class ControladorUsuario {
         if(!usuarioCreado) {
             arrayUser.add(new Usuario(correo, nombre, apellido, password, dpi, fechaC, genero, nacionalidad, usuario, telefono, rol, foto));
         }
+    }
+    
+    public boolean ValidarPassword(String pass) {
+        String b;
+        int cont1 = 0, cont2 = 0, cont3 = 0, cont4 = 0;
+        for(int i = 0; i < pass.length(); i++) {
+            b = Character.toString(pass.charAt(i));
+            if(b.matches("[A-Z]")) {
+                cont1++;
+            } else if(b.matches("[a-z]")) {
+                cont2++;
+            } else if(b.matches("[0-9]")) {
+                cont3++;
+            } else if(b.matches("[!@#$%^&*(),.?\\\":{}|<>]")) {
+                cont4++;
+            }
+        }
         
-
+        if(cont1 >= 1 && cont2 >= 1 && cont3 >= 1 && cont4 >= 1) {
+            return true;
+        }
+        return false;
     }
     
     
