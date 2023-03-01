2d0ab6e652485ecec9e78af7479fb71dfe6e0329
José Góngora <jdavidgongorao@gmail.com>
2023-02-28 22:54:00 -0600

    Se crearon metodos para kiosco

---
+++ Proyecto1/src/proyecto1/controlador/ControladorKiosco.java
@@ -0,0 +1,50 @@
+package proyecto1.controlador;
+
+import java.util.ArrayList;
+import proyecto1.modelo.Kiosco;
+
+/**
+ *
+ * @author JD
+ */
+public class ControladorKiosco {
+    
+    ArrayList<Kiosco> arrayKiosco = new ArrayList<>();
+    
+    public boolean AgregarKiosco(String codigo, String nombre, String region) {
+        
+        if(!ValidarCodigo(codigo)) {
+            arrayKiosco.add(new Kiosco(codigo, nombre, region));
+        }
+        
+        return false;
+    }
+    
+    public boolean ValidarCodigo(String codigo) {
+        
+        for(Kiosco k: arrayKiosco) {
+            if(k.getCodigo().equals(codigo)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public void EditarKiosco(String codigo, String nombre, String region) {
+        for(Kiosco k: arrayKiosco) {
+            if(k.getCodigo().equals(codigo)) {
+                if(!ValidarCodigo(codigo)) {
+                    k.setCodigo(codigo);
+                    k.setNombre(nombre);
+                    k.setCodigoRegion(region);
+                }
+            }
+        }
+    }
+    
+    public ArrayList<Kiosco> ObtenerKioscos() {
+        return arrayKiosco;
+    }
+    
+    
+}
