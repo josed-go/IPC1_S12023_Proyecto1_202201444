3655144e566096ac9242d4d9009ba53db4298f9f
José Góngora <jdavidgongorao@gmail.com>
2023-03-02 19:50:16 -0600

    Se crearon regiones por defecto

--- Proyecto1/src/proyecto1/Principal.java
+++ Proyecto1/src/proyecto1/Principal.java
@@ -1,6 +1,8 @@
 package proyecto1;
 
+import proyecto1.controlador.ControladorRegion;
 import proyecto1.controlador.ControladorUsuario;
+
 import proyecto1.vista.Login;
 
 /**
@@ -8,13 +10,16 @@ import proyecto1.vista.Login;
  * @author JD
  */
 public class Principal {
-
+    
     public static void main(String[] args) {
         ControladorUsuario userController = new ControladorUsuario();
+        ControladorRegion controladorR = new ControladorRegion();
+        controladorR.CrearRegiones();
+       
         Login login = new Login();
         userController.CrearAdmin(); // CREACION DE USUARIO ADMIN
         login.setVisible(true);
         
     }
-    
+
 }
