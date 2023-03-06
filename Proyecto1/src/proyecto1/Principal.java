733ece9cf812269a29c8c7fa54e05e3b59fa1a66
José Góngora <jdavidgongorao@gmail.com>
2023-03-05 20:12:46 -0600

    Se agregó codigo para crear departamentos quemados

--- Proyecto1/src/proyecto1/Principal.java
+++ Proyecto1/src/proyecto1/Principal.java
@@ -1,5 +1,6 @@
 package proyecto1;
 
+import proyecto1.controlador.ControladorDepartamento;
 import proyecto1.controlador.ControladorRegion;
 import proyecto1.controlador.ControladorUsuario;
 
@@ -14,8 +15,9 @@ public class Principal {
     public static void main(String[] args) {
         ControladorUsuario userController = new ControladorUsuario();
         ControladorRegion controladorR = new ControladorRegion();
+        ControladorDepartamento controladorD = new ControladorDepartamento();
         controladorR.CrearRegiones();
-       
+        controladorD.DepartamentosPrincipales();
         Login login = new Login();
         userController.CrearAdmin(); // CREACION DE USUARIO ADMIN
         login.setVisible(true);
